fa5f2df43e804be1de316453c19bdd6bdc662a0e
Zanehisako <yassinetakiko@gmail.com>
2024-10-18 00:22:53 +0100

    3d plot

--- exo2.py
+++ exo2.py
@@ -1,3 +1,4 @@
+from matplotlib.axes import Axes
 import pandas as pd
 import matplotlib.pyplot as plt 
 
@@ -22,8 +23,11 @@ print(dfo.describe())
 #calculate the new coeffiants
 print(dfo['Year'].mul(2))
 
+#average
+dfo['Average']= (dfo['Listening'] + dfo['Speaking'] + dfo['Reading'] + dfo['Writing'] / 4)
+
 #Data visualization
-fig,axes = plt.subplots(nrows=2,ncols=2)
+'''fig,axes = plt.subplots(nrows=2,ncols=2)
 
 dfo[dfo['Name'] == 'Student 1'].plot(ax=axes[0,0],kind='line',x='Year',y=['Listening','Speaking','Reading','Writing'],title='Student 1 progression')
 dfo[dfo['Name'] == 'Student 2'].plot(ax=axes[0,1],kind='line',x='Year',y=['Listening','Speaking','Reading','Writing'],title='Student 2 progression')
@@ -31,4 +35,12 @@ dfo[dfo['Name'] == 'Student 3'].plot(ax=axes[1,0],kind='line',x='Year',y=['Liste
 dfo[dfo['Name'] == 'Student 4'].plot(ax=axes[1,1],kind='line',x='Year',y=['Listening','Speaking','Reading','Writing'],title='Student 4 progression')
 
 plt.show()
-
+'''
+#3d plot
+fig = plt.figure()
+ax= fig.add_subplot(projection = '3d')
+ax.scatter(dfo['Name'].astype('category').cat.codes,dfo['Year'],dfo['Average'])
+ax.set_xlabel('Name')
+ax.set_ylabel('Year')
+ax.set_zlabel('Average')
+plt.show()
